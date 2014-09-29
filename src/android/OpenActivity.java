00e7e061a70a95ac3b094e45ffc053b1e4d1ac44
deefactorial <deefactorial@gmail.com>
2014-09-29 16:05:44 -0700

    compile source

--- src/android/OpenActivity.java
+++ src/android/OpenActivity.java
@@ -11,7 +11,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import android.util.Log;
 import android.os.Bundle;
-import technology.deefactorial.cordova.activityopen.reporting;
+import technology.deefactorial.cordova.activityopen;
 
 
 public class OpenActivity extends CordovaPlugin {
